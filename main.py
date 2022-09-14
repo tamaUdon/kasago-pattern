48f04614ff9f13fb0ab1ca8697698ebcebc22cae
tamaUdon <sakurayuki603@gmail.com>
2022-09-15 03:09:03 +0900

    fix out of index error

--- main.py
+++ main.py
@@ -37,17 +37,11 @@ def generate_grid_shape():
 # ref. https://matplotlib.org/stable/tutorials/intermediate/imshow_extent.html#sphx-glr-tutorials-intermediate-imshow-extent-py
 def get_color(idxarr):
     """Return the data color of an index."""
-    threshold, upper, lower = 0.5, 1, 0
-    # TODO: fix IndexError: index 100 is out of bounds for axis 1 with size 100
+    threshold, upper, lower = 0.5, 1, 0    
     val = data[idxarr[0], idxarr[1]] / data.max()  # normalize 0 to 1
-    print("idxarr", idxarr)
-    #print("data[idxarr]", data[idxarr])
-    print("data.max()", data.max())
-    print("color:", np.where(val > threshold, upper, lower))
     return np.where(val > threshold, upper, lower).item()  # binarize 0 or 1
 
-
-def get_around_moore_cell_states(index, distance=3, mweight=1, aweight=-0.4):
+def get_around_moore_cell_states(indexarr, distance=3, mweight=1, aweight=-0.4):
     # rx, ry = relative coordinate from [0,0] (distance=3)
     # moore neighborhood = ([rx, ry] = -1 to 1)
     # [-3,3] ... [3,3] -> y is always 3
@@ -61,21 +55,20 @@ def get_around_moore_cell_states(index, distance=3, mweight=1, aweight=-0.4):
     msum = 0
     asum = 0
     for ry in range(-distance, distance+1):
-        # skip if ry is over 100
-        if ry > 100:  # TODO: refactor to inline args
-            continue
 
         for rx in range(-distance, distance+1):
-            # skip if rx or ry is over 100
-            if rx > 100:  # TODO: refactor to inline args
-                continue
 
             # skip moore neighborhood
             if -1 <= rx & ry <= 1:
-                msum += get_color(index)
+                msum += get_color(indexarr)
                 continue
 
-            rindex = np.sum([index, [rx, ry]], axis=0)
+            # skip if index will over N
+            if (indexarr[0] or indexarr[1] >= (N - distance)):
+                continue
+
+            rindex = np.sum([indexarr, [rx, ry]], axis=0)
+            print("rindex", rindex)
             asum += get_color(rindex)
     return (msum*mweight + asum*aweight)
 
@@ -97,12 +90,12 @@ async def main():
     generate_grid_shape()
     print("data address 1", id(data))
 
+    # calculate cell dead or alive 4 times 
     for _ in range(4):
         for i in range(N):
             for j in range(N):
                 state = get_around_moore_cell_states([i, j])  # np.array
                 calculate_dead_or_alive([i, j], state)
-            print(i + " loop now!")
         plt.imshow(data,
                    cmap=cmap,
                    interpolation='none',
@@ -111,7 +104,8 @@ async def main():
         plt.show()
         await asyncio.sleep(0.1)
     print("calculate done!")
-
+    # TODO: 1回目の計算の後、モザイクが真っ黒になってしまった
 
 if __name__ == "__main__":
     asyncio.run(main())
+
